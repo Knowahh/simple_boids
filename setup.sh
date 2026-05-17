6086bc0ed1af47ff556ebb3142173dad87dfa92a
Knowahh <noahdpiontek@gmail.com>
2026-05-17 16:34:47 -0400

    setup venv fix

--- setup.sh
+++ setup.sh
@@ -14,15 +14,15 @@ if ! command -v python3 &> /dev/null; then
 fi
 
 echo "[2/3] Verifying virtual environment"
-if [ ! -d "venv" ]; then
+if [ ! -d ".venv" ]; then
     echo "Virtual environment does not exist, creating now..."
-    python3 -m venv venv
+    python3 -m venv .venv
 fi
 
 echo "[3/3] Installing/updating dependencies from requirements.txt..."
 if [ -f "requirements.txt" ]; then
-    ./venv/bin/pip install --upgrade pip
-    ./venv/bin/pip install -r requirements.txt
+    ./.venv/bin/pip install --upgrade pip
+    ./.venv/bin/pip install -r requirements.txt
 else
     echo "Error: requirements.txt not found."
     exit 1
